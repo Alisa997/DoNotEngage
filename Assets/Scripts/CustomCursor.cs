7d4c7b7efe3d09660437972604d9a1f9dff2ca51
Amelie <amelie.ry.zh@gmail.com>
2024-12-29 14:30:49 -0800

    Updated custom cursor

--- Assets/Scripts/CustomCursor.cs
+++ Assets/Scripts/CustomCursor.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Buttons : MonoBehaviour
+public class CustomCursor : MonoBehaviour
 {
+    
+    public Texture2D cursorDefault;
     
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.SetCursor(cursorDefault, Vector2.zero, CursorMode.ForceSoftware);
     }
+
 }
